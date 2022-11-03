19e0d65f5aadb8e7df3e9cc61543ad879bfe1805
mahin-wd <wdmahin.mhm@gmail.com>
2022-11-03 09:25:06 +0600

    added pagination methods

--- index.js
+++ index.js
@@ -19,7 +19,24 @@ app.get('/', (req, res) => {
 //! mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4lqljgn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const productsCollection = client.db('emaJohn').collection('products');
+        app.get('/products', async(req,res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.skip(page*size).limit(size).toArray();
+            const count = await productsCollection.estimatedDocumentCount();
+            res.send({count, products});
+        })
+    }
+    finally {
 
+    }
+}
+run().catch(err => console.error(err));
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
