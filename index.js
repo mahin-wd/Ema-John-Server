8c29165f65df224572dc95082244c2b6e30b0e43
mahin-wd <wdmahin.mhm@gmail.com>
2022-11-03 06:33:37 +0600

    mongodb connection added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv');
 const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 //* middleware
 app.use(express.json());
@@ -12,6 +14,12 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Ema-John Server');
 });
+
+
+//! mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.4lqljgn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
